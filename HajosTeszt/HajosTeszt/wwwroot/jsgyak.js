d98e0b17621ae4d949eed70df17b7646af5c120e
corvinus-bj3ksf <magyargeri10@gmail.com>
2021-03-18 18:30:02 +0100

    6het_bj3ksf

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,34 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+
+    let sorszam = document.getElementById("1feladat");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("elem");
+        sorszam.appendChild(sor);
+        sor.innerText = s;
+        sor.style.background = `rgb(${250/s}, 50, 50)`
+    }
+   
+    var faktoriális = (n) => {
+        if (n === 0 || n === 1) {
+            return 1;
+        } else {
+            return n * faktoriális(n - 1)
+        }
+    }
+
+    let hszog = document.getElementById("pascal");
+    for (var s = 0; s < 10; s++) {
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        hszog.appendChild(sor);
+        for (var o = 0; o <= s; o++) {
+            let szám = document.createElement("div");
+
+            szám.classList.add("elem");
+            sor.appendChild(szám);
+            szám.innerText = faktoriális(s) / (faktoriális(o) * faktoriális(s - o));
+        }
+    }
+}
