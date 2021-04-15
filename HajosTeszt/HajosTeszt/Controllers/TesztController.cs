76010988cc3ef20a9d5cced2fd6c1fa1f92fc30a
corvinus-bj3ksf <magyargeri10@gmail.com>
2021-04-15 12:15:09 +0200

    bj3ksf_8het

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+
+        public IActionResult M1(string szoveg)
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = szoveg.ToUpper()
+            };
+        }
+
+
+    }
+}
